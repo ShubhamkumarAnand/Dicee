96cbff15af49a16be21717519de56b5ca7cecad9
imskanand <the.anand3099@gmail.com>
2022-04-15 16:33:12 +0530

    Basic setup of the Dicee App

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Dicee'),
+        backgroundColor: Colors.red.shade900,
+      ),
+      backgroundColor: Colors.red.shade400,
+    ),
+  ));
+}
